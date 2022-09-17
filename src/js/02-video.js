1ac58cca9c5c82fc40a86251f4189409f083c8f5
ArthurS <arthurosh@gmail.com>
2022-09-17 23:50:08 +0300

    adds the executions of task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+const getTime = function () {
+  player.getCurrentTime().then(seconds => {
+    localStorage.setItem(VIDEO_CURRENT_TIME, seconds);
+  });
+};
+
+if (localStorage.getItem(VIDEO_CURRENT_TIME)) {
+  player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME));
+}
+
+player.on('timeupdate', throttle(getTime, 1000));
